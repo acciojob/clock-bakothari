fc964fb40b356a5354acb928ada19799bdb9bab9
Bhavya Kothari <129605972+bakothari@users.noreply.github.com>
2025-02-21 22:58:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your JS code here. If required.
+function updateTimer() {
+            const timerElement = document.getElementById("timer");
+            const now = new Date();
+            timerElement.textContent = now.toLocaleString();
+        }
+        
+        setInterval(updateTimer, 1000);
+        updateTimer(); 
